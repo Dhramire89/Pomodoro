7d0ee7e10da93d24a761e59343d0b3b09b300989
dramirez <dramirezrrzz@gmail.com>
2025-03-19 22:09:18 -0600

    Agregando colores

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, Platform, SafeAreaView, View } from 'react-native';
 import { useState } from 'react';
+import Header from './.expo/src/components/Header';
 //Platform: se usa solo para aplicar a una plataforma 
 
-const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"]
+//const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"]
+const colors = ["#F4A261", "#2A9D8F", "#8ECAE6"]
 
 
 export default function App() {
@@ -13,9 +15,11 @@ export default function App() {
   const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "LONG"); 
 
   return (
-    <SafeAreaView style={styles.container}> 
+    <SafeAreaView style={[styles.container, {backgroundColor: colors[currentTime]}]}> 
         <View style={styles.ViewContainer}>
         <Text style={styles.text} >Pomodoro</Text>
+        <Text style={styles.text} >{time}</Text>
+        <Header currentTime={currentTime} setCurrentTime={setCurrentTime} setTime={setTime}/>
         <StatusBar style='auto'/>
         </View>
       
@@ -34,6 +38,5 @@ const styles = StyleSheet.create({
   ViewContainer :{
     paddingTop: Platform.OS ==="android" && 30,
     paddingTop: 30,
-    paddingHorizontal: 10, 
   },
 });
