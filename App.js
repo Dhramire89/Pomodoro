fecdf3f7d3dc56e4c7a57bb92610f035906ebc21
dramirez <dramirezrrzz@gmail.com>
2025-03-20 20:06:41 -0600

    Timer corriendo correctamente

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Platform, SafeAreaView, View } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, Platform, SafeAreaView, View, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
 import Header from './.expo/src/components/Header';
 import Timer from './.expo/src/components/Timer';
 //Platform: se usa solo para aplicar a una plataforma 
@@ -14,6 +14,27 @@ export default function App() {
   const [isWorking, setIsWorking] = useState(false);
   const [time, setTime] = useState(25 * 60);
   const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "LONG");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      // correr el timer
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime - 1);
+      }, 1000);
+    } else {
+      // detener el timer
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, time]);
+
+  function handleStarStop() {
+    setIsActive(!isActive);
+
+  }
 
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: colors[currentTime] }]}>
@@ -21,6 +42,9 @@ export default function App() {
         <Text style={styles.text} >Pomodoro</Text>
         <Header currentTime={currentTime} setCurrentTime={setCurrentTime} setTime={setTime} />
         <Timer time={time} />
+        <TouchableOpacity onPress={handleStarStop} style={styles.TouchableOpacity}>
+          <Text style={styles.TouchableOpacityText}>{isActive ? "Stop" : "Start"}</Text>
+        </TouchableOpacity>
         <StatusBar style='auto' />
       </View>
 
@@ -41,6 +65,19 @@ const styles = StyleSheet.create({
   ViewContainer: {
     paddingTop: Platform.OS === "android" && 30,
     paddingTop: 30,
-    flex: 0.3,
+    flex: 0.4,
   },
+  TouchableOpacityText: {
+    fontSize: 20,
+    fontWeight: "bold",
+
+  },
+  TouchableOpacity: {
+    marginTop: 20,
+    backgroundColor: "#ffff",
+    marginHorizontal: 16,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: "center"
+  }
 });
